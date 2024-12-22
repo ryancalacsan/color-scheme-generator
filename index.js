4afb06c5ddbed39734d18bd3b171174b5442f173
ryancalacsan <65645127+ryancalacsan@users.noreply.github.com>
2024-12-22 14:15:22 -0600

    refactored code and cleaned up css

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const schemeSelectEl = document.getElementById("scheme-select")
 const GenerateBtn = document.getElementById("generate-btn")
 const schemeEl = document.getElementById("scheme")
 
-GenerateBtn.addEventListener("click", () => {
+// set random color on load
+const randomHexColorCode = () => {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16)
+  return "#" + n.slice(0, 6)
+}
+
+pickerEl.value = randomHexColorCode()
+generateScheme()
+
+GenerateBtn.addEventListener("click", generateScheme)
+
+function generateScheme() {
   let pickedColor = pickerEl.value.slice(1)
   let pickedScheme = schemeSelectEl.value
   let schemeHtml = []
 
-  console.log(pickedColor)
-  console.log(pickedScheme)
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${pickedColor}&mode=${pickedScheme}`
   )
@@ -17,9 +26,15 @@ GenerateBtn.addEventListener("click", () => {
     .then((data) => {
       schemeHtml = data.colors.map((color) => {
         return `
-        <div class="color" style="background-color: ${color.hex.value}"></div>
-        `
+          <div class="color" style="background-color: ${color.hex.value}">
+            <p>#123456</p>
+            <p>color name</p>
+          </div>
+          `
       })
       schemeEl.innerHTML = schemeHtml.join(" ")
     })
-})
+}
+
+// TODO
+// Set font color based on backgruond color
